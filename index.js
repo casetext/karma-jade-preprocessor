d777519e568050b7c6094732f212ab40ae2b46f7
Harry Schmidt <me@goldibex.com>
2014-01-12 14:19:07 -0800

    Disable renaming (we don't need it)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var createJadePreprocessor = function(logger, basePath) {
     var processed = null;
 
     log.debug('Processing "%s".', file.originalPath);
-    file.path = file.originalPath.replace(/\.jade$/, '.html');
+//    file.path = file.originalPath.replace(/\.jade$/, '.html');
 
     var templateName = file.originalPath.replace(/^.*\/([^\/]+)\.jade$/, '$1');
 
